088bbbc77667c79816297ba938fedba5f056a661
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-05-02 17:42:52 +0430

    add 4 more functions

--- main.cpp
+++ main.cpp
@@ -13,21 +13,25 @@ Word *createWord(string);
 
 void toLower(string &);
 
-void addSynonym(Word *&node, Word *synonym);
+void addWordOrSynonym(Word *&, Word *);
 
+Word *checkWordExist(Word *, const string &);
+
+void mainAddWOrd(Word *&);
+
+void deleteWord(Word *&, const string &);
+
+void mainDeleteWord(Word *&);
+
+void printAll(Word *);
+
+void deleteAllSynonyms(Word *synHead);
+
+Word *HEAD = nullptr;
 
 int main() {
-    string s1 = "glad";
-    string s2 = "happy";
-    string s3 = "fine";
-    string s4 = "good";
-    Word *temp = createWord(s1);
-    addSynonym(temp->syn, createWord(s2));
-    addSynonym(temp->syn, createWord(s3));
-    addSynonym(temp->syn, createWord(s4));
-    while (temp->syn) {
-        cout << temp->syn->value << " ";
-        temp->syn = temp->syn->next;
+    for (int i = 0; i < 2; ++i) {
+        mainAddWOrd(HEAD);
     }
 
 
@@ -46,7 +50,7 @@ void toLower(string &s) {
     }
 }
 
-void addSynonym(Word *&node, Word *synonym) {
+void addWordOrSynonym(Word *&node, Word *synonym) {
     if (node == nullptr) {
         node = synonym;
         return;
@@ -55,7 +59,7 @@ void addSynonym(Word *&node, Word *synonym) {
     Word *current = node;
 
     while (current) {
-        if (synonym->value.compare(current->value) == -1) {
+        if (synonym->value < current->value) {
             if (current == node) {
                 synonym->next = current;
                 node = synonym;
@@ -72,6 +76,89 @@ void addSynonym(Word *&node, Word *synonym) {
     synonym->next = nullptr;
 }
 
+Word *checkWordExist(Word *head, const string &word) {
+    while (head) {
+        if (head->value == word)
+            return head;
+        head = head->next;
+    }
+    return nullptr;
+}
 
+void mainAddWOrd(Word *&head) {
+    cout << "Enter the word you want to add :";
+    string word;
+    cin >> word;
+    int n;
+    cout << "Enter the number of synonyms :";
+    cin >> n;
+    Word *tempNode = checkWordExist(head, word);
+    if (tempNode) {
+        for (int i = 0; i < n; ++i) {
+            string tempWord;
+            cin >> tempWord;
+            addWordOrSynonym(tempNode->syn, createWord(tempWord));
+        }
+    } else {
+        tempNode = createWord(word);
+        for (int i = 0; i < n; ++i) {
+            string tempWord;
+            cin >> tempWord;
+            addWordOrSynonym(tempNode->syn, createWord(tempWord));
+        }
+        addWordOrSynonym(head, tempNode);
+    }
+}
 
+void deleteWord(Word *&head, const string &word) {
+    Word *prev = nullptr;
+    Word *current = head;
+    if (head == nullptr) {
+        cout << "The dictionary is empty!";
+        return;
+    } else {
+        while (current) {
+            if (word == current->value) {
+                if (current == head && current->next == nullptr)
+                    head = nullptr;
+                else if (current == head)
+                    head = head->next;
+                else
+                    prev->next = current->next;
 
+                deleteAllSynonyms(current->syn);
+                delete current;
+                return;
+            }
+            prev = current;
+            current = current->next;
+        }
+    }
+}
+
+void printAll(Word *head) {
+    while (head) {
+        cout << head->value << " : ";
+        while (head->syn) {
+            cout << head->syn->value << " ";
+            head->syn = head->syn->next;
+        }
+        head = head->next;
+        cout << endl;
+    }
+}
+
+void deleteAllSynonyms(Word *synHead) {
+    Word *temp;
+    while (synHead) {
+        temp = synHead;
+        synHead = synHead->next;
+        delete temp;
+    }
+}
+
+void mainDeleteWord(Word *&head) {
+    string word;
+    cout << "Enter the word you want to delete from the dictionary :";
+    deleteWord(head, word);
+}
